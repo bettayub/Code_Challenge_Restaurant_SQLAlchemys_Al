3cda7de01cb4a3cd5534a2c0c5de8ee91985c75d
bettayub <bettayub19@gmail.com>
2023-09-05 00:36:51 +0300

    Defining the Customer model

--- models.py
+++ models.py
@@ -1,3 +1,17 @@
+# Import necessary modules from SQLAlchemy
+# Create the SQLAlchemy engine to connect to the database
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+
+
+# Define the base class for declarative models
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    # Defining columns for the table
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    price = Column(Integer)
